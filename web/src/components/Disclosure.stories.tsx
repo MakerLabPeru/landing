4c2bc1851b614096ff66247e57cae84af140da33
Pedro Palacios Avila <wesitos@makerlabperu.org>
2022-12-02 18:22:13 -0500

    Add storybook interactions for Disclosure component

--- web/src/components/Disclosure.stories.tsx
+++ web/src/components/Disclosure.stories.tsx
@@ -1,5 +1,12 @@
-import {Disclosure} from '~/components';
+import {expect} from '@storybook/jest';
+import {
+  userEvent,
+  within,
+  waitFor,
+} from '@storybook/testing-library';
 import {ComponentStory, ComponentMeta} from '@storybook/react';
+
+import {Disclosure} from '~/components';
 
 export default {
   title: 'Components/Disclosure',
@@ -19,7 +26,6 @@ Open.args = {
     personas con la misma pasión de aprender y enseñar.`,
 };
 
-
 export const Closed = Template.bind({});
 
 Closed.args = {
@@ -27,4 +33,26 @@ Closed.args = {
   title: '¿Qué es el MakerLab?',
   children: `MakerLab es una comunidad tecnológica que une a varias
     personas con la misma pasión de aprender y enseñar.`,
-}
+};
+
+export const Default = Template.bind({});
+
+Default.args = {
+  title: 'Titulo',
+  children: `Texto de contenido`,
+};
+Default.play = async ({args, canvasElement}) => {
+  const canvas = within(canvasElement);
+
+  expect(canvas.queryByText(args.children)).not.toBeVisible();
+
+  await userEvent.click(canvas.getByRole('button'));
+
+  await waitFor(async () =>
+    expect(await canvas.getByText(args.children)).toBeVisible(),
+  );
+
+  await userEvent.click(canvas.getByRole('button'));
+
+  expect(canvas.queryByText(args.children)).not.toBeVisible();
+};
