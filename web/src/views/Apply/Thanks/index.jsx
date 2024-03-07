17cb59ba72f1f309edce0d62796f6cf36b3ed7a0
Pedro Palacios Avila <wesitos@makerlabperu.org>
2024-03-06 19:44:21 -0500

    fix: Thanks Page build error

--- web/src/views/Apply/Thanks/index.jsx
+++ web/src/views/Apply/Thanks/index.jsx
@@ -1,26 +1,23 @@
-import Layout from '~/views/Layout';
 import Image from 'next/image';
-import Button from '~/components/Button';
-import bannerThanks from '../../../../public/images/thanks-banner.png';
+import {Button} from '~/components/Button';
+import bannerThanks from '~public/images/thanks-banner.png';
 
-function Thanks() {
-  return (
-    <Layout>
-      <div className="flex items-center justify-center p-6">
-        <Image src={bannerThanks} height={300} alt=" banner" />
-      </div>
-      <h1 className="text-4xl text-white font-bold text-center pb-6">
-        Gracias por postular!
-      </h1>
-      <h2 className="text-1.5xl text-white text-center">
-        Tu aplicación ha sido correctamente enviada, nos pondremos en contacto
-        contigo lo más antes posible.
-      </h2>
-      <div className="flex items-center justify-center p-6">
-        <Button>Volver al inicio</Button>
-      </div>
-    </Layout>
-  );
-}
+const Thanks = () => (
+  <>
+    <div className="flex items-center justify-center p-6">
+      <Image src={bannerThanks} height={300} alt=" banner" />
+    </div>
+    <h1 className="text-4xl text-white font-bold text-center pb-6">
+      Gracias por postular!
+    </h1>
+    <h2 className="text-1.5xl text-white text-center">
+      Tu aplicación ha sido correctamente enviada, nos pondremos en contacto
+      contigo lo más antes posible.
+    </h2>
+    <div className="flex items-center justify-center p-6">
+      <Button href="/">Volver al inicio</Button>
+    </div>
+  </>
+);
 
 export default Thanks;
