a82d79da7b5985273530795fc472abb28aedca2c
Pedro Palacios Avila <wesitos@makerlabperu.org>
2024-03-06 20:14:01 -0500

    chore: Improve styles

--- web/src/components/LinkField.tsx
+++ web/src/components/LinkField.tsx
@@ -1,8 +1,6 @@
-import {ErrorMessage, Field} from "formik";
-import Image from "next/image";
-import {styles} from "../styles/tailwindGlobals";
-import deleteIcon from "../../public/icons/close.svg";
-import addIcon from "../../public/icons/plus.svg";
+import {ErrorMessage, Field} from 'formik';
+import {MdAdd, MdRemove} from 'react-icons/md';
+import {styles} from '../styles/tailwindGlobals';
 
 interface LinkFieldProps {
   name: string;
@@ -11,39 +9,39 @@ interface LinkFieldProps {
   push: (obj: unknown) => void;
 }
 
-export const LinkField = ({name, index, remove, push}: LinkFieldProps) => (
-  <div className="grid grid-cols-1 space-y-2 mt-2 md:space-y-1">
-    <div className="flex items-center space-x-2">
-      <Field className={styles.dynamicField} name={`${name}.${index}.url`}/>
-      {index !== 0 && (
-        <button
-          type="button"
-          className="p-1 text-gray-500 hover:text-gray-800 focus:outline-none focus:text-gray-800"
-          onClick={() => remove(index)}
-        >
-          <Image
-            src={deleteIcon}
-            className="w-8 px-1 md:w-14 md:px-3"
-            alt="Delete"
-          />
-        </button>
-      )}
-      {index === 0 && (
-        <button type="button" onClick={() => push({url: ''})}>
-          <Image
-            src={addIcon}
-            className="h-10 w-10 px-2 md:h-16 md:w-16 md:px-4"
-            alt="Add"
-          />
-        </button>
-      )}
+export const LinkField = ({name, index, remove, push}: LinkFieldProps) => {
+  const buttonClass =
+    'className="p-1 text-neutral-50 hover:text-neutral-200 focus:outline-none focus:text-neutral-200"';
+  return (
+    <div className="grid grid-cols-1 space-y-2 mt-2 md:space-y-1">
+      <div className="flex items-center space-x-2">
+        <Field className={styles.dynamicField} name={`${name}.${index}.url`} />
+        {index !== 0 && (
+          <button
+            type="button"
+            className={buttonClass}
+            onClick={() => remove(index)}
+          >
+            <MdRemove className="w-8 px-1 md:w-14 md:px-3" />
+          </button>
+        )}
+        {index === 0 && (
+          <button
+            type="button"
+            className={buttonClass}
+            onClick={() => push({url: ''})}
+          >
+            <MdAdd className="h-10 w-10 px-2 md:h-16 md:w-16 md:px-4" />
+          </button>
+        )}
+      </div>
+      <div>
+        <ErrorMessage
+          name={`${name}.${index}.url`}
+          component="div"
+          className={styles.errorMsg}
+        />
+      </div>
     </div>
-    <div>
-      <ErrorMessage
-        name={`${name}.${index}.url`}
-        component="div"
-        className={styles.errorMsg}
-      />
-    </div>
-  </div>
-)
+  );
+};
