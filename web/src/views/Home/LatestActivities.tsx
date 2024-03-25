ac29b96e0b6f626e2a40e8feec8c226b3f1bef1e
Jhair Guzman <jhairguz.t@gmail.com>
2024-03-25 15:35:36 -0500

    feat: define latest activities component

--- web/src/views/Home/LatestActivities.tsx
+++ web/src/views/Home/LatestActivities.tsx
@@ -1,30 +1,9 @@
-import Image from "next/image";
-import { Button } from "../../components";
 
 const LatestActivities = () => (
-  <div className="">
-    <div className="">
-      <Image
-        src="/images/home-methodology-1.png"
-        width={403}
-        height={390}
-        alt="makerlab"
-      />
-
-      <div className="">
-        <h2 className="">
-          Nuestra metodología
-        </h2>
-        <p className="">
-          <b>En MakerLab no existen clases.</b><br/>
-          Cada uno es libre de ir a su propio ritmo e interés,
-          pero siempre va a encontrar apoyo en uno de nuestros Coaches.
-        </p>
-        <Button href="/">
-          Conócenos
-        </Button>
-      </div>
-    </div>
+  <div className="md:container md:mx-auto">
+    <h2 className="text-white font-bold text-xl">
+      Actividades recientes
+    </h2>
   </div>
 );
 
--- web/src/views/Home/index.tsx
+++ web/src/views/Home/index.tsx
@@ -13,7 +13,7 @@ const Home = () => (
     <Hero />
     <Support />
     <About />
-    {/*<LatestActivities />*/}
+    <LatestActivities />
   </div>
 );
 
